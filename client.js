7534c70bbcd2976f7044d7a103666c4e3dc420e1
Diego Baeza <diegob12@live.com>
2022-06-18 16:17:20 -0500

    add function to retrieve data from add employee interface to add to table and clear interface data after doing so

--- client.js
+++ client.js
@@ -1,5 +1,35 @@
 $(document).ready(readyWhenever);
 
+function addEmployeeDetails() {
+    let insertName = $('#nameInput').val();
+    let insertLastInput = $('#lastInput').val();
+    let insertIdInput = $('#idInput').val();
+    let insertTitleInput = $('#titleInput').val();
+    let insertSalaryInput = $('#salaryInput').val();
+
+    $('#infoTable').append(`
+    <tr>
+                <td>${insertName}</td>
+                <td>${insertLastInput}</td>
+                <td>${insertIdInput}</td>
+                <td>${insertTitleInput}</td>
+                <td>${insertSalaryInput}</td>
+                <td class="column-delete"><button class="delete-btn">Delete</button></td>
+            </tr>
+    `)
+
+    emptyInterface();
+}
+
+function emptyInterface() {
+    $('#nameInput').val('');
+    $('#lastInput').val('');
+    $('#idInput').val('');
+    $('#titleInput').val('');
+    $('#salaryInput').val('');
+}
+
 function readyWhenever () {
     //console.log('jQuery is running');
+    $('#submitButton').on('click', addEmployeeDetails);
 }
